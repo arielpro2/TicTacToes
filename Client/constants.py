f0eb5e3312baae0a7f0a844386bec9daa177d946
amit.pinchasi <amit.pinchasi@watchful.ai>
2023-06-24 11:35:33 +0300

    reformated constants.py

--- Client/constants.py
+++ Client/constants.py
@@ -2,15 +2,18 @@ class Request:
     CREATE_ROOM = 0
     JOIN_ROOM = 1
 
+
 class UserOptions:
     JOIN_ROOM = 'J'
     CREATE_ROOM = 'C'
+
 
 class Game:
     FPS = 60
     SCREEN_WIDTH, SCREEN_HEIGHT = 900, 900
 
     EMPTY_CELL = 0
+
 
 class Colors:
     BLACK = (0, 0, 0)
